0d7beb7a782258770107778451f79fa8be02f594
James Nanas <jamescbn@yahoo.com>
2025-05-13 15:43:21 -0400

    Modified the emotion_detector function to format the output.

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def emotion_detector(text_to_analyze: str):
@@ -6,4 +7,20 @@ def emotion_detector(text_to_analyze: str):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     res = requests.post(url, json=input_json, headers=headers)
-    return res.text
+
+    # Format the response via json to turn into a Python dictionary.
+    formatted_res = json.loads(res.text)
+
+    # Extract the emotion scores and find the dominant emotion.
+    emotion_scores = formatted_res['emotionPredictions'][0]['emotion']
+    dominant_emotion = None
+    max_score = 0
+    emotion_output = {}
+    for emotion, score in emotion_scores.items():
+        emotion_output[emotion] = score
+        if max_score < score:
+            max_score = score
+            dominant_emotion = emotion
+
+    emotion_output['dominant_emotion'] = dominant_emotion
+    return emotion_output
